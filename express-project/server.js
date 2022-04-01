3d2302f4f02b2a19c709bd13f816229ebe1bb19e
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-01 18:02:51 +0900

    express-project/85

--- express-project/server.js
+++ express-project/server.js
@@ -2,8 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+const friends = [
+  {
+    id: 30,
+    name: 'Stephen Curry',
+  },
+  {
+    id: 11,
+    name: 'Klay Thompson',
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const friend = friends.filter((friend) => friend.id === id);
+
+  if (!friend || friend.length === 0) {
+    console.log('comming');
+    return res.status(404).json({ error: 'Friends does not found.' });
+  }
+  res.status(200).json(friend);
 });
 
 const PORT = 3000;
