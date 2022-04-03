6b5ec0deb78779d9b023b8a16306e1953cb75b5d
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-03 11:29:54 +0900

    nasa-project/112

--- nasa-project/server/src/models/planets.model.js
+++ nasa-project/server/src/models/planets.model.js
@@ -1,3 +1,50 @@
-const planets = [];
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
+const results = [];
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      // __diername: D:\dev\udemy_personal\ztm_node\nasa-project\server\src\models
+      // .. .. で2つdirを上がって
+      // data, kepler_data.csvを指定する。
+      // .. は / がOSによって違うのを吸収するための方法らしい。
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          results.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log(`${results.length} habitable planet found.`);
+        resolve(results);
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: results,
+};
--- nasa-project/server/src/routes/planets/planets.controller.js
+++ nasa-project/server/src/routes/planets/planets.controller.js
@@ -1,4 +1,9 @@
-const planets = require('../../models/planets.model');
+// { planets } しないと配列に戻らない。
+// module.exports = {
+//   loadPlanetsData,
+//   planets: results,
+// };
+const { planets } = require('../../models/planets.model');
 
 function getAllPlanets(req, res) {
   return res.status(200).json(planets);
