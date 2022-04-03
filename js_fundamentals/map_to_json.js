affab176c762357941fb6e8a5302b042986f5b11
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-04 06:06:44 +0900

    nasa-project/119

---
+++ js_fundamentals/map_to_json.js
@@ -0,0 +1,28 @@
+const launches = new Map();
+
+const launch_1 = {
+  flightNumber: 100,
+  mission: 'Kepler Explortation X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('2022/9/4'),
+  destination: 'Kepler-442 b',
+  customer: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true,
+};
+
+const launch_2 = {
+  flightNumber: 101,
+  mission: 'Kepler Explortation X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('2022/9/4'),
+  destination: 'Kepler-442 b',
+  customer: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch_1.flightNumber, launch_1);
+launches.set(launch_2.flightNumber, launch_2);
+
+console.log(Array.from(launches.values()));
