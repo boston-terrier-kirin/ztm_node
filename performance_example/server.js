ba7bcb6cc8e599d0f49452d8f48d097aa934fd91
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-04 17:47:03 +0900

    performance-example/148

--- performance_example/server.js
+++ performance_example/server.js
@@ -9,11 +9,14 @@ function delay(duration) {
 
 app.get('/', (req, res) => {
   res.send(`performance example: ${process.pid}`);
+  console.log(`performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(10000);
   res.send(`Ding ding ding!: ${process.pid}`);
+  console.log(`Ding ding ding!: ${process.pid}`);
 });
 
+console.log("Listening on port 3000");
 app.listen(3000);
