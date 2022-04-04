1fd901ad9266562a68c16006e0dfcae67d6b70d4
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-04 14:12:33 +0900

    nasa-project/134

---
+++ js_fundamentals/invalid_date.js
@@ -0,0 +1,4 @@
+console.log(new Date('hello'));
+
+// 2022/3/1になる
+console.log(new Date('2022/2/30'));
--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -1,18 +1,102 @@
-describe('Test GET /launches', () => {
-  test('It should respond with 200 success', () => {
-    const response = 200;
+const request = require('supertest');
+const app = require('../../app');
 
-    expect(response).toBe(200);
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
   });
 });
 
 describe('Test POST /launches', () => {
-  test('It should respond with 200 success', () => {
-    const response = 200;
+  const completeLaunchData = {
+    mission: 'ZTM156',
+    rocket: 'Explorer ZTM',
+    launchDate: '2022/1/1',
+    target: 'Kepler-186 f',
+  };
 
-    expect(response).toBe(200);
+  const launchDataWithoutDate = {
+    mission: 'ZTM156',
+    rocket: 'Explorer ZTM',
+    target: 'Kepler-186 f',
+  };
+
+  const launchDataWithoutMission = {
+    rocket: 'Explorer ZTM',
+    launchDate: '2022/1/1',
+    target: 'Kepler-186 f',
+  };
+
+  const launchDataWithoutRocket = {
+    mission: 'ZTM156',
+    launchDate: '2022/1/1',
+    target: 'Kepler-186 f',
+  };
+
+  const launchDataWithoutTarget = {
+    mission: 'ZTM156',
+    rocket: 'Explorer ZTM',
+    launchDate: '2022/1/1',
+  };
+
+  const launchWithInvalidDate = {
+    mission: 'ZTM156',
+    rocket: 'Explorer ZTM',
+    launchDate: 'Hello',
+    target: 'Kepler-186 f',
+  };
+
+  test('It should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const reqestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(reqestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test('It should catch missing required property', () => {});
-  test('It should catch invalid date', () => {});
+  test('It should catch missing required property', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property.',
+    });
+
+    await request(app)
+      .post('/launches')
+      .send(launchDataWithoutMission)
+      .expect('Content-Type', /json/)
+      .expect(400);
+    await request(app)
+      .post('/launches')
+      .send(launchDataWithoutRocket)
+      .expect('Content-Type', /json/)
+      .expect(400);
+    await request(app)
+      .post('/launches')
+      .send(launchDataWithoutTarget)
+      .expect('Content-Type', /json/)
+      .expect(400);
+  });
+
+  test('It should catch invalid date', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchWithInvalidDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({ error: 'Invalid launch date' });
+  });
 });
